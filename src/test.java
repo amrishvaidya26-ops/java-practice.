80b90674739c2497b8ad1435a0d345aec5659daf
Amrish Vaidya <amrishvaidya26@gmail.com>
2026-07-27 13:54:10 +0530

    First Commit

---
+++ src/test.java
@@ -0,0 +1,88 @@
+import jdk.jfr.Percentage;
+import java.util.Scanner;
+public class test {
+
+    public static void main (String [] args ) {
+
+//        System.out.println("Welcome to java Programming");
+//        System.out.println("My name is'Amrish Vaidya'. ");
+//        System.out.println("I am Learning java basics.");
+//    }
+//}
+
+
+    // Question no. 2
+
+// public static void main (String [] args) {
+//
+//     String name = " Name : Amrish Vaidya";
+//     int age =  21;
+//     String city = "City : CHH. Sambhaji Nagar";
+//     Double Percentage = 76.82;
+//
+//     System.out.println(name);
+//     System.out.println(age);
+//     System.out.println(city);
+//     System.out.println(Percentage);
+// }
+//
+// }
+
+
+    // Question no 3rd //
+
+
+//        int A,B,Add,Mul,Div,sub,mod ;
+//
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.println("Enter 1st Number");
+//        A = scan.nextInt();
+//        System.out.println("Enter 2nd Number");
+//        B = scan.nextInt();
+//
+//        Add = A+B;
+//        System.out.println("Additon="+ Add );
+//
+//        sub = A-B;
+//        System.out.println("Substraction="+sub);
+//
+//        Mul = A*B;
+//        System.out.println("Multiplication="+ Mul);
+//
+//        Div = A/B;
+//        System.out.println("Divisible=" + Div);
+//
+//
+//
+//
+//
+//
+//
+
+    }
+}
+
+//    //  Question no 4 //
+//    public static void main(String[] args) {
+//
+//        int age;
+//        double price;
+//        char percentage;
+//        boolean Nonstop;
+//        byte ;
+//        short;
+//        long;
+//        float;
+//
+//
+//        System.out.println("Enter student age ");
+//        System.out.println("Enter the  price");
+//        System.out.println("Enter the Percentage");
+//        System.out.println("Enter the Raining");
+//
+//
+//
+//
+//    }
+//}
